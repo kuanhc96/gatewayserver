da8e64c1d417d9e0f10f36e731bac121eaed7fdc
Kuan <chenkuansk@ad.garmin.com>
2025-11-18 12:41:42 +0800

    callback function receiving requests

---
+++ src/main/java/com/example/gatewayserver/controller/CallbackController.java
@@ -0,0 +1,59 @@
+package com.example.gatewayserver.controller;
+
+import java.util.Map;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@RestController
+public class CallbackController {
+	@PostMapping("/callback")
+	public ResponseEntity<?> callback(
+            @RequestParam(required = false) String code,
+			@RequestParam(required = false) String state,
+            @RequestParam(required = false) String error) {
+		System.out.println(code);
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+		body.add("grant_type", "authorization_code");
+		body.add("code", code);
+		body.add("state", state);
+		body.add("redirect_uri", "http://localhost:8080/callback");
+		body.add("client_id", "fe-client");
+		body.add("client_secret", "secret1");
+
+		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, headers);
+		ResponseEntity<Map> tokenResponse = restTemplate.exchange(
+				"http://localhost:9000/oauth2/token",
+				HttpMethod.POST,
+				request,
+				Map.class
+		);
+		if (tokenResponse.getStatusCode() == HttpStatus.OK) {
+			HttpHeaders responseHeaders = new HttpHeaders();
+			responseHeaders.add("Access-Control-Allow-Origin", "*");
+			return ResponseEntity.ok().headers(responseHeaders).body(tokenResponse.getBody());
+		} else {
+			return ResponseEntity.status(tokenResponse.getStatusCode()).body("Failed to retrieve access token");
+		}
+	}
+}
