0ae7b4e95b632289c5a1799b95be2fd18faf4474
Kuan <chenkuansk@ad.garmin.com>
2025-12-04 23:10:09 +0800

    redirect from Gateway -> authorization -> loginForm -> callbackPage -> gateway callback works

--- src/main/java/com/example/gatewayserver/controller/OAuthController.java
+++ src/main/java/com/example/gatewayserver/controller/OAuthController.java
@@ -1,5 +1,6 @@
 package com.example.gatewayserver.controller;
 
+import java.net.URI;
 import java.util.Map;
 
 import org.springframework.http.HttpEntity;
@@ -8,15 +9,36 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class OAuthController {
+	@GetMapping("/authorize")
+	public void authorize(ServerHttpResponse response) {
+		URI uri = UriComponentsBuilder.fromUriString("http://localhost:9000/oauth2/authorize")
+				.queryParam("client_id", "fe-client")
+				.queryParam("redirect_uri", "http://localhost:8080/callback")
+				.queryParam("response_type", "code")
+				.queryParam("scope", "openid")
+				.queryParam("state", "abcde")
+				.build().toUri();
+
+		response.getHeaders().setLocation(uri);
+		response.getHeaders().setAccessControlAllowOrigin("http://localhost:8080");
+		response.setStatusCode(HttpStatus.FOUND);
+	}
+
 	@PostMapping("/callback")
 	public ResponseEntity<?> callback(
             @RequestParam(required = false) String code,
